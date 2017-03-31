9da0c03d5fd7b606e353d9d82f9a028e9b840743
William Pettersson <william.pettersson@gmail.com>
2017-03-31 13:26:00 +1100

    Complete merge to CMake

---
+++ scripts/checkResults.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+EXECUTABLE=$1
+TEST=$2
+OPTS=$3
+TESTNAME=$(basename ${TEST} .lp)
+TESTDIR=$(dirname ${TEST})
+OUTFILE=$(mktemp ${TESTNAME}.XXX)
+${EXECUTABLE} -p ${TEST} -o ${OUTFILE}
+diff -w -I 'seconds\|solved\|Using' ${TESTDIR}/${TESTNAME}.out ${OUTFILE}
+RES=$?
+rm ${OUTFILE}
+exit ${RES}
